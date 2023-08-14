b7a72ca7574b898abf47bca7dc4646aa97441086
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-08-15 00:49:52 +0600

    3rd

--- script/login.js
+++ script/login.js
@@ -4,7 +4,11 @@ document.getElementById("btn-submit").addEventListener("click", function () {
   // step 2: get the email address inside the email input field
   const emailField = document.getElementById("user-email");
   const email = emailField.value;
-  const passField = document.getElementById('user-pass')
-  const pass = passField.value
-  console.log(pass)
+  const passField = document.getElementById("user-pass");
+  const pass = passField.value;
+  if (email === "said@mail.com" && pass === "1234") {
+    location.href = "bank.html";
+  } else {
+    alert('Wrong Input')
+  }
 });
